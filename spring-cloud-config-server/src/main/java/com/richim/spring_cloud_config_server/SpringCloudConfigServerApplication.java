5a449712a0070d1cf76b2c5b2e3138249582cadf
Rishikeshp8899 <patilrishikeshm88@gmail.com>
2025-04-29 23:44:10 +0530

    "first Microservice add"

--- spring-cloud-config-server/src/main/java/com/richim/spring_cloud_config_server/SpringCloudConfigServerApplication.java
+++ spring-cloud-config-server/src/main/java/com/richim/spring_cloud_config_server/SpringCloudConfigServerApplication.java
@@ -2,8 +2,12 @@ package com.richim.spring_cloud_config_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigurationProperties
 @SpringBootApplication
+@EnableConfigServer
 public class SpringCloudConfigServerApplication {
 
 	public static void main(String[] args) {
