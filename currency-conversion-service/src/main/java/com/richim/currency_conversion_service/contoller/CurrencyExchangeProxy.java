acf31b2491557cd140a112ee94abcfeae5d2582e
Rishikeshp8899 <patilrishikeshm88@gmail.com>
2025-05-25 18:18:23 +0530

    "add api-gateway"

--- currency-conversion-service/src/main/java/com/richim/currency_conversion_service/contoller/CurrencyExchangeProxy.java
+++ currency-conversion-service/src/main/java/com/richim/currency_conversion_service/contoller/CurrencyExchangeProxy.java
@@ -6,7 +6,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.richim.currency_conversion_service.bean.CurrencyConversion;
 
-@FeignClient(name = "currency-exchange-service", url = "http://127.0.0.1:7001")
+//@FeignClient(name = "currency-exchange-service", url = "http://127.0.0.1:7001") before eureka register
+@FeignClient(name = "currency-exchange-service")
 public interface CurrencyExchangeProxy {
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public CurrencyConversion CurrencyExchangeController(@PathVariable String from, @PathVariable String to);
